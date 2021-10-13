d343812223441cbe6210ddaa2431c0fb2c5efef7
wkd3475 <wkd3475@gmail.com>
2021-10-13 20:30:05 +0900

    FEAT : Chat Page UI
    Compose the Chat Page UI

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -5,7 +5,18 @@ import './Chat.css'
 const Chat = () => {
   return (
     <div className='wrapper'>
-      <h2>Chat Page : {getCookie('name')}</h2>
+      <div className='body'>
+        <div className='top-container'>
+          <h2>Name : {getCookie('name')}</h2>
+          <button className='btn'>Logout</button>
+        </div>
+        <div className='mid-container'>
+          <h2>display messages</h2>
+        </div>
+        <div className='bottom-container'>
+          <h2>input message</h2>
+        </div>
+      </div>
     </div>
   );
 }
