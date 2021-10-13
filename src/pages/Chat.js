ed0b9e8d5831c8f6ed6902de53e6750da7c7f404
wkd3475 <wkd3475@gmail.com>
2021-10-13 20:42:16 +0900

    FEAT : Complete (b-3-1), (b-3-2)

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { getCookie } from '../utils/cookie'
+import { removeCookie, getCookie } from '../utils/cookie'
 import './Chat.css'
 
 const Chat = () => {
+    const name = getCookie('name');
+    const handleLogout = () => {
+        removeCookie('name', name);
+        window.location.replace("/login");
+      };
   return (
     <div className='wrapper'>
       <div className='body'>
         <div className='top-container'>
           <h2>Name : {getCookie('name')}</h2>
-          <button className='btn'>Logout</button>
+          <button className='btn' onClick={handleLogout}>Logout</button>
         </div>
         <div className='mid-container'>
           <h2>display messages</h2>
